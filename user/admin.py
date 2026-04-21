a6e4435bdbea5a449608776b6d64f6d69cc6c74b
artem <artem90078@gmail.com>
2026-04-21 18:12:10 +0300

    Add custom user admin configuration

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from user.models import User
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ("email", "is_staff", "is_superuser")
+    list_filter = ("is_staff", "is_superuser", "is_active")
+    search_fields = ("email",)
+    ordering = ("email",)
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "password", "is_staff", "is_active"),
+        }),
+    )
