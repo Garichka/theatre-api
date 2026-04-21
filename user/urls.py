59a9e9a5eb5bdc5d32ee58c7645ea227ccc5e927
artem <artem90078@gmail.com>
2026-04-21 16:22:44 +0300

    Add custom User model and authentication serializers

---
+++ user/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from user.views import CreateUserView, CreateTokenView
+
+
+urlpatterns = [
+    path("register/", CreateUserView.as_view(), name="register"),
+    path("token/", CreateTokenView.as_view(), name="token"),
+]
+
+app_name = "user"
